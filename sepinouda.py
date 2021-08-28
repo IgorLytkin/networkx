7fcf15da6add6098e4b41c550aefb2122a68ce93
Igor Lytkin <igor.lytkin.2020@gmail.com>
2021-08-29 04:03:17 +0700

    New star in Universe - Sepinoud Azimi

--- sepinouda.py
+++ sepinouda.py
@@ -2,15 +2,20 @@
 
 import networkx as nx
 
-def sepinouda():
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-    G = nx.Graph()
-    G.add_edge('A', 'B', weight=13, relation='friend')
-    G.add_edge('B', 'C', weight=9, relation='family')
-    G.add_edge('B', 'D', weight=7, relation='friend')
-    G.add_edge('E', 'B', weight=10, relation='friend')
-    G.add_edge('E', 'A', weight=1, relation='enemy')
-    G.add_edge('F', 'B', weight=13, relation='family')
-    G.edges(data=True)
+def main():
+    g = nx.Graph()
+    g.add_edge('A', 'B', weight=13, relation='friend')
+    g.add_edge('B', 'C', weight=9, relation='family')
+    g.add_edge('B', 'D', weight=7, relation='friend')
+    g.add_edge('E', 'B', weight=10, relation='friend')
+    g.add_edge('E', 'A', weight=1, relation='enemy')
+    g.add_edge('F', 'B', weight=13, relation='family')
+    g.edges(data=True)
+
+    g.add_node('A',role='Trader')
+    g.add_node('B',role='Analyst')
+    g.add_node('C',role='Manager')
+    g.nodes(data=True)
+
+    nx.draw(g)
