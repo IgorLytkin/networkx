072291a78bb75d15fded91995b7b19d95c66051f
Igor Lytkin <igor.lytkin.2020@gmail.com>
2021-08-29 12:31:12 +0700

    New star in Universe - Sepinoud Azimi

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Introduction to NetworkX in Python (https://www.youtube.com/watch?v=flwcAf1_1RU)
+
+import sepinouda
+import tutorial
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    #  tutorial.main()
+    sepinouda.main()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
